b4c7d424b70800631b803bd546de3120755fb753
KrLite <68179735+KrLite@users.noreply.github.com>
2023-01-22 02:24:28 +0800

    mixin plugin

--- src/main/java/net/krlite/equator/EquatorLib.java
+++ src/main/java/net/krlite/equator/EquatorLib.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 public class EquatorLib implements ModInitializer {
 	public static final String MOD_ID = "equator";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final boolean DEBUG = false;
 
 	@Override
 	public void onInitialize() {
--- src/main/java/net/krlite/equator/mixin/debug/EquatorDebugger.java
+++ src/main/java/net/krlite/equator/mixin/debug/EquatorDebugger.java
@@ -1,4 +1,4 @@
-package net.krlite.equator.mixin;
+package net.krlite.equator.mixin.debug;
 
 import net.krlite.equator.debug.CanvasScreen;
 import net.minecraft.client.MinecraftClient;
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(MinecraftClient.class)
 public class EquatorDebugger {
 	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V", ordinal = 1))
-private void onSetScreen(MinecraftClient client, Screen screen) {
+	private void onSetScreen(MinecraftClient client, Screen screen) {
 		client.setScreen(new CanvasScreen(screen));
 	}
 }
