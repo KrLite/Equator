4c6f5957263f535de8b436ee290196f9b38847fe
KrLite <68179735+KrLite@users.noreply.github.com>
2023-01-24 14:09:58 +0800

    1.12

--- src/main/java/net/krlite/equator/EquatorLib.java
+++ src/main/java/net/krlite/equator/EquatorLib.java
@@ -2,13 +2,14 @@ package net.krlite.equator;
 
 import net.fabricmc.api.ModInitializer;
 import net.krlite.equator.debug.CanvasScreen;
+import net.krlite.equator.util.IdentifierBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EquatorLib implements ModInitializer {
 	public static final String MOD_ID = "equator";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static final boolean DEBUG = false;
+	public static final boolean DEBUG = true;
 
 	@Override
 	public void onInitialize() {
