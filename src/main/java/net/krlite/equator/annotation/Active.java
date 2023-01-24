8c4e00fc34d442157f21adfef269d93735552d4c
KrLite <68179735+KrLite@users.noreply.github.com>
2023-01-24 11:51:47 +0800

    migrate

--- src/main/java/net/krlite/equator/annotation/Active.java
+++ src/main/java/net/krlite/equator/annotation/Active.java
@@ -5,7 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Active(willMigrate = true)
 @Target(ElementType.TYPE_USE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Active {
+	boolean willMigrate() default false;
 }
--- src/main/java/net/krlite/equator/annotation/See.java
+++ src/main/java/net/krlite/equator/annotation/See.java
@@ -3,6 +3,7 @@ package net.krlite.equator.annotation;
 
 import java.lang.annotation.Repeatable;
 
+@Active(willMigrate = true)
 @Repeatable(Sees.class)
 public @interface See {
 	Class<?> value();
